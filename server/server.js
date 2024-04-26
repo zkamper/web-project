e7172be0f61d361080588047c2db1f009a7ec90f
Ionut Calin <ionut.calin645@gmail.com>
2024-04-26 13:21:25 +0300

    init project structure

--- server/server.js
+++ server/server.js
@@ -1,13 +1,9 @@
 const http = require('http');
-const fs = require('fs');
 const PORT = process.env.PORT || 3456;
 
 const server = http.createServer((request, response) => {
-
     response.writeHead(200, {"Content-Type" : "text/html"});
     response.end("<h1>Hello, world !<h1>");
-
-    console.log("it's ok to be ghe");
 });
 
 server.listen(PORT, function(error){
